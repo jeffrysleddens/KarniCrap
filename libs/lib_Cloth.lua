8fac39a0ab4b25dedfca56f0f549eb270a5215b3
Jeffry Sleddens <jeffry@acid6.scene.org>
2014-10-20 10:55:11 +0200

    Added windwool cloth

--- libs/lib_Cloth.lua
+++ libs/lib_Cloth.lua
@@ -3,8 +3,9 @@ clothList = {
 	["2592"] 	= "Wool Cloth",
 	["4306"] 	= "Silk Cloth",
 	["4338"] 	= "Mageweave Cloth",
-	["14047"] = "Runecloth",
-	["21877"] = "Netherweave Cloth",
-	["33470"] = "Frostweave Cloth",
-	["53010"] = "Embersilk"
+	["14047"]	= "Runecloth",
+	["21877"]	= "Netherweave Cloth",
+	["33470"]	= "Frostweave Cloth",
+	["53010"]	= "Embersilk",
+	["72988"]	= "Windwool Cloth"
 }
