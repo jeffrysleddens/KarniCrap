c188bec93a44273da3aa69feabb14e755ecd9bce
Jeffry Sleddens <jeffry.sleddens@gmail.com>
2014-10-22 18:26:40 +0200

    Added Zen Master levels and fixed formatting.

--- libs/lib_Tradeskills.lua
+++ libs/lib_Tradeskills.lua
@@ -1,125 +1,128 @@
-tradeskillList = {
-
+tradeskillList	= {
 -- Disenchant
-	[13262] = "Disenchant", 		-- No levels
+	[13262]		= "Disenchant", 	-- No levels
 -- Engineering
-	[4036]  = "Engineering", 		-- Apprentice
-	[4037]  = "Engineering", 		-- Journeyman
-	[4038]  = "Engineering", 		-- Expert
-	[12656] = "Engineering", 		-- Artisan
-	[30350] = "Engineering", 		-- Master
-	[51306] = "Engineering", 		-- Grand Master
-	[82774] = "Engineering", 		-- Illustrious
-	[49383] = "Engineering", 		-- Corpses
+	[4036]		= "Engineering", 	-- Apprentice
+	[4037]		= "Engineering", 	-- Journeyman
+	[4038]		= "Engineering", 	-- Expert
+	[12656]		= "Engineering", 	-- Artisan
+	[30350]		= "Engineering", 	-- Master
+	[51306]		= "Engineering", 	-- Grand Master
+	[82774]		= "Engineering", 	-- Illustrious
+	[110403]	= "Engineering",	-- Zen Master
+	[49383]		= "Engineering", 	-- Corpses
 -- Herb Gathering
-	[2366]  = "Herb Gathering", -- Apprentice
-	[2368]  = "Herb Gathering", -- Journeyman
-	[3570]  = "Herb Gathering", -- Expert
-	[11993] = "Herb Gathering", -- Artisan
-	[28695] = "Herb Gathering", -- Master
-	[50300] = "Herb Gathering", -- Grand Master
-	[74519] = "Herb Gathering", -- Illustrious
+	[2366]		= "Herb Gathering",	-- Apprentice
+	[2368]		= "Herb Gathering",	-- Journeyman
+	[3570]		= "Herb Gathering",	-- Expert
+	[11993]		= "Herb Gathering",	-- Artisan
+	[28695]		= "Herb Gathering",	-- Master
+	[50300]		= "Herb Gathering",	-- Grand Master
+	[74519]		= "Herb Gathering",	-- Illustrious
+	[110413]	= "Herb Gathering",	-- Zen Master
 -- Mining
-	[2575] 	= "Mining", 				-- Apprentice
-	[2576] 	= "Mining", 				-- Journeyman
-	[3564] 	= "Mining", 				-- Expert
-	[10248] = "Mining", 				-- Artisan
-	[29354] = "Mining", 				-- Master
-	[50310] = "Mining", 				-- Grand Master
-	[74517] = "Mining", 				-- Illustrious
+	[2575] 		= "Mining", 		-- Apprentice
+	[2576] 		= "Mining", 		-- Journeyman
+	[3564] 		= "Mining", 		-- Expert
+	[10248]		= "Mining", 		-- Artisan
+	[29354]		= "Mining", 		-- Master
+	[50310]		= "Mining", 		-- Grand Master
+	[74517]		= "Mining", 		-- Illustrious
+	[102161]	= "Mining",		-- Zen Master
 -- Skinning
-	[8613]  = "Skinning", 			-- Apprentice
-	[8617]  = "Skinning", 			-- Journeyman
-	[8618]  = "Skinning",				-- Expert
-	[10768] = "Skinning", 			-- Artisan
-	[32678] = "Skinning", 			-- Master
-	[50305] = "Skinning", 			-- Grand Master
-	[74522] = "Skinning", 			-- Illustrious
+	[8613]		= "Skinning", 		-- Apprentice
+	[8617]		= "Skinning", 		-- Journeyman
+	[8618]		= "Skinning",		-- Expert
+	[10768]		= "Skinning", 		-- Artisan
+	[32678]		= "Skinning", 		-- Master
+	[50305]		= "Skinning", 		-- Grand Master
+	[74522]		= "Skinning", 		-- Illustrious
+	[102216]	= "Skinning",		-- Zen Master
 -- Milling
-	[51005] = "Milling", 				-- No levels
+	[51005]		= "Milling", 		-- No levels
 -- Prospecting
-	[31252] = "Prospecting"			-- No levels
-}
-
+	[31252]		= "Prospecting"		-- No levels
 -- Fishing (Should this one count as a tradeskill?)
---	[7620] 	= "Fishing", 				-- Apprentice
---	[7731] 	= "Fishing", 				-- Journeyman
---	[7732] 	= "Fishing", 				-- Expert
---	[18248] = "Fishing", 				-- Artisan
---	[33095] = "Fishing", 				-- Master
---	[51294] = "Fishing", 				-- Grand Master
---	[88868] = "Fishing", 				-- Illustrious
+--	[7620] 		= "Fishing", 		-- Apprentice
+--	[7731] 		= "Fishing", 		-- Journeyman
+--	[7732] 		= "Fishing", 		-- Expert
+--	[18248]		= "Fishing", 		-- Artisan
+--	[33095]		= "Fishing", 		-- Master
+--	[51294]		= "Fishing", 		-- Grand Master
+--	[88868]		= "Fishing", 		-- Illustrious
+--	[110410]	= "Fishing",		-- Zen Master
 -- Archaeology (Not sure if this triggers a loot window)
---	[78670] = "Archaeology", 		-- Apprentice
---	[88961] = "Archaeology", 		-- Journeyman
---	[89718] = "Archaeology", 		-- Expert
---	[89719] = "Archaeology", 		-- Artisan
---	[89720] = "Archaeology", 		-- Master
---	[89721] = "Archaeology", 		-- Grand Master
---	[89722] = "Archaeology", 		-- Illustrious
+--	[78670]		= "Archaeology", 	-- Apprentice
+--	[88961]		= "Archaeology", 	-- Journeyman
+--	[89718]		= "Archaeology", 	-- Expert
+--	[89719]		= "Archaeology", 	-- Artisan
+--	[89720]		= "Archaeology", 	-- Master
+--	[89721]		= "Archaeology", 	-- Grand Master
+--	[89722]		= "Archaeology", 	-- Illustrious
+--	[110393]	= "Archaeology",	-- Zen Master
 -- Alchemy (No loot window)
---	[2259] 	= "Alchemy", 				-- Apprentice
---	[3101] 	= "Alchemy", 				-- Journeyman
---	[3464] 	= "Alchemy", 				-- Expert
---	[11611] = "Alchemy", 				-- Artisan
---	[28596] = "Alchemy", 				-- Master
---	[51304] = "Alchemy", 				-- Grand Master
---	[80731] = "Alchemy", 				-- Illustrious
+--	[2259] 		= "Alchemy", 		-- Apprentice
+--	[3101] 		= "Alchemy", 		-- Journeyman
+--	[3464] 		= "Alchemy", 		-- Expert
+--	[11611]		= "Alchemy", 		-- Artisan
+--	[28596]		= "Alchemy", 		-- Master
+--	[51304]		= "Alchemy", 		-- Grand Master
+--	[80731]		= "Alchemy", 		-- Illustrious
+--	[105206]	= "Alchemy",		-- Zen Master
 -- Blacksmithing (No loot window)
---	[2018]  =	"Blacksmithing", 	-- Apprentice
---	[3100]  =	"Blacksmithing", 	-- Journeyman
---	[3538]  =	"Blacksmithing", 	-- Expert
---	[9785]  =	"Blacksmithing", 	-- Artisan
---	[29844] = "Blacksmithing", 	-- Master
---	[51300] = "Blacksmithing", 	-- Grand Master
---	[76666] = "Blacksmithing", 	-- Illustrious
+--	[2018]		= "Blacksmithing", 	-- Apprentice
+--	[3100]		= "Blacksmithing", 	-- Journeyman
+--	[3538]		= "Blacksmithing", 	-- Expert
+--	[9785]		= "Blacksmithing", 	-- Artisan
+--	[29844]		= "Blacksmithing", 	-- Master
+--	[51300]		= "Blacksmithing", 	-- Grand Master
+--	[76666]		= "Blacksmithing", 	-- Illustrious
+--	[110396]	= "Blacksmithing",	-- Zen Master
 -- Enchanting (No loot window)
---	[7411]  = "Enchanting", 		-- Apprentice
---	[7412]  = "Enchanting", 		-- Journeyman
---	[7413]  = "Enchanting", 		-- Expert
---	[13920] = "Enchanting", 		-- Artisan
---	[28029] = "Enchanting", 		-- Master
---	[51313] = "Enchanting", 		-- Grand Master
---	[74258] = "Enchanting", 		-- Illustrious
+--	[7411]		= "Enchanting", 	-- Apprentice
+--	[7412]		= "Enchanting", 	-- Journeyman
+--	[7413]		= "Enchanting", 	-- Expert
+--	[13920]		= "Enchanting", 	-- Artisan
+--	[28029]		= "Enchanting", 	-- Master
+--	[51313]		= "Enchanting", 	-- Grand Master
+--	[74258]		= "Enchanting", 	-- Illustrious
+--	[110400]	= "Enchanting",		-- Zen Master
 -- Inscription (Not sure if this triggers a loot window)
---	[45357] = "Inscription", 		-- Apprentice
---	[45358] = "Inscription", 		-- Journeyman
---	[45359] = "Inscription", 		-- Expert
---	[45360] = "Inscription", 		-- Artisan
---	[45361] = "Inscription", 		-- Master
---	[45363] = "Inscription", 		-- Grand Master
---	[86008] = "Inscription", 		-- Illustrious
+--	[45357]		= "Inscription", 	-- Apprentice
+--	[45358]		= "Inscription", 	-- Journeyman
+--	[45359]		= "Inscription", 	-- Expert
+--	[45360]		= "Inscription", 	-- Artisan
+--	[45361]		= "Inscription", 	-- Master
+--	[45363]		= "Inscription", 	-- Grand Master
+--	[86008]		= "Inscription", 	-- Illustrious
+--	[110417]	= "Inscription",	-- Zen Master
 -- Jewelcrafting (Not sure if this triggers a loot window)
---	[25229] = "Jewelcrafting", 	-- Apprentice
---	[25230] = "Jewelcrafting", 	-- Journeyman
---	[28894] = "Jewelcrafting", 	-- Expert
---	[28895] = "Jewelcrafting", 	-- Artisan
---	[28897] = "Jewelcrafting", 	-- Master
---	[51311] = "Jewelcrafting", 	-- Grand Master
---	[73318] = "Jewelcrafting", 	-- Illustrious
+--	[25229]		= "Jewelcrafting", 	-- Apprentice
+--	[25230]		= "Jewelcrafting", 	-- Journeyman
+--	[28894]		= "Jewelcrafting", 	-- Expert
+--	[28895]		= "Jewelcrafting", 	-- Artisan
+--	[28897]		= "Jewelcrafting", 	-- Master
+--	[51311]		= "Jewelcrafting", 	-- Grand Master
+--	[73318]		= "Jewelcrafting", 	-- Illustrious
+--	[110420]	= "Jewelcrafting",	-- Zen Master
 -- Leatherworking (No loot window)
---	[2108]  = "Leatherworking", -- Apprentice
---	[3104]  = "Leatherworking", -- Journeyman
---	[3811]  = "Leatherworking", -- Expert
---	[10662] = "Leatherworking", -- Artisan
---	[32549] = "Leatherworking", -- Master
---	[51302] = "Leatherworking", -- Grand Master
---	[81199] = "Leatherworking", -- Illustrious
+--	[2108]		= "Leatherworking",	-- Apprentice
+--	[3104]		= "Leatherworking",	-- Journeyman
+--	[3811]		= "Leatherworking",	-- Expert
+--	[10662]		= "Leatherworking",	-- Artisan
+--	[32549]		= "Leatherworking",	-- Master
+--	[51302]		= "Leatherworking",	-- Grand Master
+--	[81199]		= "Leatherworking",	-- Illustrious
+--	[110423]	= "Leatherworking",	-- Zen Master
 -- Tailoring (No loot window)
---	[3908] 	= "Tailoring", 			-- Apprentice
---	[3909] 	= "Tailoring", 			-- Journeyman
---	[3910] 	= "Tailoring", 			-- Expert
---	[12180] = "Tailoring", 			-- Artisan
---	[26790] = "Tailoring", 			-- Master
---	[51309] = "Tailoring", 			-- Grand Master
---	[75156] = "Tailoring", 			-- Illustrious
+--	[3908] 		= "Tailoring", 		-- Apprentice
+--	[3909] 		= "Tailoring", 		-- Journeyman
+--	[3910] 		= "Tailoring", 		-- Expert
+--	[12180]		= "Tailoring", 		-- Artisan
+--	[26790]		= "Tailoring", 		-- Master
+--	[51309]		= "Tailoring", 		-- Grand Master
+--	[75156]		= "Tailoring", 		-- Illustrious
+--	[110426]	= "Tailoring",		-- Zen Master
 -- Smelting (No loot window)
---	[2656] 	= "Smelting", 			-- No levels
-
-
-
-
-
-
-
-
+--	[2656] 		= "Smelting", 		-- No levels
+}
