7b5fb24442823edecb3dcbd175fd90b369097c17
Jeffry Sleddens <jeffry.sleddens@gmail.com>
2014-10-22 18:29:24 +0200

    Added Mists of Pandaria water.

--- libs/lib_Water.lua
+++ libs/lib_Water.lua
@@ -2,27 +2,30 @@
 
 waterList = {
 	-- Classic --
-	["159"]		= 0, 		-- Refreshing Spring Water
-	["17404"] = 5,		-- Blended Bean Brew
-	["1179"] 	= 5, 		-- Ice Cold Milk
-	["9451"] 	= 15,		-- Bubbling Water
-	["1205"] 	= 15,		-- Melon Juice
-	["4791"] 	= 25,		-- Enchanted Water
-	["1708"] 	= 25,		-- Sweet Nectar
-	["1645"] 	= 35,		-- Moonberry Juice
-	["8766"] 	= 45,		-- Morning Glory Dew
-	["18300"] = 55, 	-- Hyjal Nectar
+	["159"]		= 0,		-- Refreshing Spring Water
+	["17404"]	= 5,		-- Blended Bean Brew
+	["1179"]	= 5,		-- Ice Cold Milk
+	["9451"]	= 15,		-- Bubbling Water
+	["1205"]	= 15,		-- Melon Juice
+	["4791"]	= 25,		-- Enchanted Water
+	["1708"]	= 25,		-- Sweet Nectar
+	["1645"]	= 35,		-- Moonberry Juice
+	["8766"]	= 45,		-- Morning Glory Dew
+	["18300"]	= 55,		-- Hyjal Nectar
 	-- Burning Crusade --
-	["28399"] = 60,	 	-- Filtered Draenic Water
-	["30457"] = 65, 	-- Gilneas Sparkling Water
-	["27860"] = 65, 	-- Purified Draenic Water
+	["28399"]	= 60,		-- Filtered Draenic Water
+	["30457"]	= 65,		-- Gilneas Sparkling Water
+	["27860"]	= 65,		-- Purified Draenic Water
 	-- Wrath of the Lich King --
-	["33444"] = 70, 	-- Pungent Seal Whey
-	["33445"] = 75, 	-- Honeymint Tea
-	["41731"] = 75,		-- Yeti Milk
+	["33444"]	= 70,		-- Pungent Seal Whey
+	["33445"]	= 75,		-- Honeymint Tea
+	["41731"]	= 75,		-- Yeti Milk
 	-- Cataclysm --
-	["49365"] = 5, 		-- Briaroot Brew
-	["59230"] = 80,		-- Fungus Squeezings
-	["59029"] = 80,		-- Greasy Whale Milk
-	["58256"] = 80	 	-- Sparkling Oasis Water
+	["49365"]	= 5,		-- Briaroot Brew
+	["59230"]	= 80,		-- Fungus Squeezings
+	["59029"]	= 80,		-- Greasy Whale Milk
+	["58256"]	= 80,		-- Sparkling Oasis Water
+	-- Mists of Pandaria --
+	["89683"]	= 85,		-- Hozen Cuervo
+	["81415"]	= 90		-- Plum Wine
 }
