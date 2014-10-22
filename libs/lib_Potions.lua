c5721dbdb618d5e00fab1f098f31799163eb81b4
Jeffry Sleddens <jeffry.sleddens@gmail.com>
2014-10-22 18:04:31 +0200

    Fixed formatting.

--- libs/lib_Potions.lua
+++ libs/lib_Potions.lua
@@ -1,27 +1,24 @@
 HealingPotionList = {
-	["118"] 	= 0, 		-- Minor Healing Potion
-	["858"] 	= 3, 		-- Lesser Healing Potion
-	["929"] 	= 12, 	-- Healing Potion
-	["1710"] 	= 21, 	-- Greater Healing Potion
-	["3928"] 	= 35, 	-- Superior Healing Potion
-	["13446"] = 45, 	-- Major Healing Potion
-	["32905"] = 55, 	-- Bottled Nethergon Vapor
-	["22829"] = 55, 	-- Super Healing Potion
-	["33447"] = 70 		-- Runic Healing Potion
--- Cataclysm 
-	--["57191"] = 80 -- Mythical Healing Potion (Leave this out? only seems to be from fished container)
+	["118"]		= 0,	-- Minor Healing Potion
+	["858"]		= 3,	-- Lesser Healing Potion
+	["929"]		= 12,	-- Healing Potion
+	["1710"]	= 21,	-- Greater Healing Potion
+	["3928"]	= 35,	-- Superior Healing Potion
+	["13446"]	= 45,	-- Major Healing Potion
+	["32905"]	= 55,	-- Bottled Nethergon Vapor
+	["22829"]	= 55,	-- Super Healing Potion
+	["33447"]	= 70,	-- Runic Healing Potion
+	--["57191"]	= 80	-- Mythical Healing Potion (Leave this out? only seems to be from fished container)
 }
 
-ManaPotionList = {
-	["2455"] 	= 5, 		-- Minor Mana Potion
-	["3385"] 	= 14, 	-- Lesser Mana Potion
-	["3827"] 	= 22, 	-- Mana Potion
-	["6149"] 	= 31, 	-- Greater Mana Potion
-	["13443"] = 41, 	-- Superior Mana Potion
-	["13444"] = 49, 	-- Major Mana Potion
-	["32902"] = 55, 	-- Bottled Nethergon Energy
-	["22832"] = 55, 	-- Super Mana Potion
-	["33448"] = 70		-- Runic Mana Potion
--- Cataclysm
-
+ManaPotionList	= {
+	["2455"]	= 5,	-- Minor Mana Potion
+	["3385"]	= 14,	-- Lesser Mana Potion
+	["3827"]	= 22,	-- Mana Potion
+	["6149"]	= 31,	-- Greater Mana Potion
+	["13443"]	= 41,	-- Superior Mana Potion
+	["13444"]	= 49,	-- Major Mana Potion
+	["32902"]	= 55,	-- Bottled Nethergon Energy
+	["22832"]	= 55,	-- Super Mana Potion
+	["33448"]	= 70	-- Runic Mana Potion
 }
