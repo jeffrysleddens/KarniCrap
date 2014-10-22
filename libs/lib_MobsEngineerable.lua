69d0a911b7f88360c00ba6267397e773cbc8a55e
Jeffry Sleddens <jeffry.sleddens@gmail.com>
2014-10-22 17:46:46 +0200

    Full list of salvagable mobs, big thanks to Blitzed@curse!

--- libs/lib_MobsEngineerable.lua
+++ libs/lib_MobsEngineerable.lua
@@ -1,10 +1,53 @@
--- woefully incomplete. There is no way to currently search for salvagable mobs on wowhead.com
--- any ideas?
+-- Further additions can be made using the following link http://www.wowhead.com/npcs?filter=cr=39:44;crs=2:1;crv=0:0
 mobsEngineerableList = {
-	["25793"] = true, --55-D Collect-a-tron
-	["25758"] = true, --Defendo-tank 66D
-	["25814"] = true, --Fizzcrank Mechagnome
-	["25752"] = true, --Scavenge-bot 004-A8
-	["25792"] = true, --Scavenge-bot 005-B6	
-	["25753"] = true --Sentry-bot 57-K
+	["14224"]	= true, -- 7:XT
+	["573"]		= true, -- Foe Reaper 4000
+	["11684"]	= true, -- Goblin Deforester
+	["115"]		= true, -- Harvest Reaper
+	["114"]		= true, -- Harvest Watcher
+	["4260"]	= true, -- Venture Co. Shredder
+	-- Wrath of the Lich King --
+	["33113"]	= true, -- Flame Leviathan
+	["33293"]	= true, -- XT-002 Deconstructor
+	["25793"]	= true, -- 55-D Collect-a-tron
+	["34183"]	= true, -- Arachnopod Destroyer
+	["27641"]	= true, -- Centrifuge Construct
+	["34193"]	= true, -- Clockwork Sapper
+	["25758"]	= true, -- Defendo-tank 66D
+	["32500"]	= true, -- Dirkee
+	["25814"]	= true, -- Fizzcrank Mechagnome
+	["32358"]	= true, -- Fumblub Gearwind
+	["33432"]	= true, -- Leviathan Mk II
+	["29724"]	= true, -- Library Guardian
+	["27972"]	= true, -- Lightning Construct
+	["29389"]	= true, -- Mechagnome Laborer
+	["34267"]	= true, -- Parts Recovery Technician
+	["27970"]	= true, -- Raging Construct
+	["25752"]	= true, -- Scavenge-bot 004-A8
+	["25792"]	= true, -- Scavenge-bot 005-B6
+	["25753"]	= true, -- Sentry-bot 57-K
+	["28835"]	= true, -- Stormforged Construct
+	["29382"]	= true, -- Stormforged Reaver
+	["29380"]	= true, -- Stormforged War Golem
+	["27971"]	= true, -- Unrelenting Construct
+	["34273"]	= true, -- XB-488 Disposalbot
+	["34271"]	= true, -- XD-175 Compactobot
+	["34269"]	= true, -- XR-949 Salvagebot
+	-- Cataclysm --
+	["43778"]	= true, -- Foe Reaper 5000
+	["42226"]	= true, -- Crushcog Battle Suit
+	["42291"]	= true, -- Crushcog Sentry-Bot
+	["46098"]	= true, -- Deathblade Shredder
+	["45185"]	= true, -- Diamond-Blade Shredder
+	["48259"]	= true, -- Irontree Shredder
+	["39376"]	= true, -- KTC Oil Bot
+	["45399"]	= true, -- Optimo
+	["44576"]	= true, -- Proto-Strider
+	["39354"]	= true, -- Steamwheedle Shark
+	-- Mists of Pandaria --
+	["58787"]	= true, -- Big Bessa
+	["66327"]	= true, -- Garrosh'ar Shredder
+	["64479"]	= true, -- Gatecrusher
+	["67285"]	= true, -- Jungle Shredder
+	["67371"]	= true, -- Shredmaster Packle
 }
